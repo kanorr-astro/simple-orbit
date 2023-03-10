a3f4680bc25cd3146a27af5d2df51067929a8c42
kanorr-astro <kanorr.astro@gmail.com>
2023-03-10 15:46:59 -0600

    add param

---
+++ src/simple-orbit.py
@@ -0,0 +1,14 @@
+#Simple Orbit
+
+#Calculate and plot simple orbit for 2-body w/ body 1 at origin w/ mass m1 and body 2 orbiting w/ mass m2
+
+
+#Body Parameters
+m1 = 6*10^24                        #kg (approx earth mass)
+m2 = 400                            #kg
+
+#Initial positions/velo
+r1 = [0, 0, 0]                      #km
+r2 = [0, 700, 0]                    #km
+v1 = [0, 0, 0]                      #km/s
+v2 = [200, 0, 0]                    #km/s
