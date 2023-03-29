12b250f48c4984bd667b6dca64692552447f827e
kanorr-astro <kanorr.astro@gmail.com>
2023-03-29 15:40:51 -0500

    basic calcs and plot

--- src/simple-orbit.py
+++ src/simple-orbit.py
@@ -1,5 +1,10 @@
 #Simple Orbit
 
+import numpy as np
+import math as math
+import matplotlib.pyplot as plt
+import utility as util
+
 #Calculate and plot simple orbit for 2-body w/ body 1 at origin w/ mass m1 and body 2 orbiting w/ mass m2
 
 
@@ -7,5 +12,39 @@
 mu = 1                              #DU^3 / TU^2
 
 #Initial positions/velo
-r = [0, 0, 0]                       #DU
-v = [0, 0, 0]                       #DU / TU
+r = [4, 0, 0]                       #DU
+v = [0, 0.4, 0]                       #DU / TU
+
+
+h = np.cross(r,v)
+SME = util.spec_mech_energy(r,v,mu)
+a = - mu / (2*SME)
+p = util.magnitude(h)**2 / mu
+e = math.sqrt(1 - (p / a))
+
+
+
+print("Angular Momentum Vector: ", h)
+print("Specific Mechanical Energy: ", SME)
+print("Semi-Major Axis: ", a)
+print("Semi-Latus Rectum: ", p)
+print("Eccentricity: ", e)
+
+
+nu_vals = np.linspace(0, 2*math.pi, 100)
+rad = []
+for nu in nu_vals:
+    rad.append(util.radius(p,e,nu))
+
+
+
+fig,ax = plt.subplots(subplot_kw={'projection':'polar'})
+ax.plot(nu_vals,rad)
+ax.set_rmax(np.max(rad))
+ax.set_rticks(np.linspace(0,round(np.max(rad)+1)),5)
+ax.grid(True)
+
+ax.set_title("test")
+plt.show()
+
+
--- src/utility.py
+++ src/utility.py
@@ -1,2 +1,14 @@
 #Utility Functions for simple-orbit
+import numpy as np
+import math as math
 
+
+def magnitude(vector):
+    return math.sqrt(sum(pow(element, 2) for element in vector))
+
+def spec_mech_energy(r,v,mu):
+    SME = (magnitude(v)**2 / 2) - (mu / magnitude(r))
+    return SME   
+
+def radius(p, e, nu):
+    return p / (1 + e * math.cos(nu))
